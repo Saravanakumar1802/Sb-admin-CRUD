f210e1879f9d568b94b0a5903c54547fb3f3a567
SARAVANAKUMAR <saravana1802kumar@gmail.com>
2022-12-16 18:14:07 +0530

    sb-admin CRUD initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ useState } from 'react';
+import Slidbar from './component/Slidbar';
+import Dashboard from './component/Dashboard';
+import AddUser from './component/AddUser';
+import { EditUser } from './component/EditUser';
+import NestedExample from './component/NestedExample';
+import Account from './component/nestedComponents/Account';
+import Profile from './component/nestedComponents/Profile';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+
+export const userContext = React.createContext();
 
 function App() {
+  let data = {
+    earningsMonthly: "40,000",
+    earningsYearly: "215,000",
+    task: "50",
+    pendingRequest: "18"
+  }
+
+  let [users, setUsers] = useState([{
+    name: "yyy",
+    email: 'yyy@gmail.com',
+    mobile: "12345678",
+    batch: "B38WE",
+    timings: '11amto1pm'
+  },
+  {
+    name: "zzz",
+    email: 'zzzgmail.com',
+    mobile: "12345678",
+    batch: "B38WE",
+    timings: '11amto1pm'
+  },
+  {
+    name: "sk",
+    email: 'sklogan@gmail.com',
+    mobile: "12345678",
+    batch: "B38WE",
+    timings: '11amto1pm'
+  }])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='wrapper'>
+      <BrowserRouter>
+        <Slidbar />
+        <userContext.Provider value={{ data, users, setUsers }}>
+          <Routes>
+
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/add-user' element={<AddUser users={users} setUsers={setUsers} />} />
+            <Route path='/edit-user/:id' element={<EditUser users={users} setUsers={setUsers} />} />
+            <Route path='nested-example' element={<NestedExample />} >
+              <Route path='account' element={<Account />} />
+              <Route path='profile' element={< Profile />} />
+            </Route>
+
+            <Route path='*' element={<Navigate to={'/dashboard'} />} />
+          </Routes>
+        </userContext.Provider>
+      </BrowserRouter>
+
+
+
+
+
     </div>
   );
 }
